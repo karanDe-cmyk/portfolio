f5393e8c0e8c0794fc717dd9f4da8393c0f2d188
karan <kk899168@gmail.com>
2025-04-16 00:39:19 +0530

    Fix: Add logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import './Responsive.css';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import logo from '../images/logo.png';
 import rightBanner from '../images/home-right.webp';
 import whatsapp from '../images/whatsapp-icon.png'
